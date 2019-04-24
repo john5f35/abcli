1cec882dbd6df4ab27f05cb9cf6b7510ec0ab569
John Zhang <john.u5f35@gmail.com>
2019-04-24 21:53:24 +1000

    some ideas about commands

---
+++ accbook/cli/__init__.py
@@ -0,0 +1,12 @@
+# CLI
+#
+# Commands:
+#   - import transactions <json>
+#   - import budget <json>
+#   - show transaction (?)
+#   - show account [name] [date-from] [date-to] [aggregation:week|fortnight|*month*|quarter|year]
+#       Shows balance, average in aggregation method, between two dates
+#   - graph account [name] [date-from] [date-to] [aggregation:...]
+#   - show budget [name] [account]
+#       Shows progress & summary of a named budget
+#   - show projection [name] [unit] [aggregation:...]
